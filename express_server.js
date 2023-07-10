9fef25b9ffa5338618886d6b23aa9635d1163e7e
Benson <bensonwlee@gmail.com>
2023-07-10 17:39:16 -0400

    added partials/header

--- express_server.js
+++ express_server.js
@@ -27,7 +27,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
